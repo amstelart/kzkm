7a4bc4f7d65eb2fd034c9e6c5a0afe54ceb8484e
Артем <amstelart@MacBook-Pro-Artem.local>
2018-02-19 11:32:42 +0300

    add modal

--- src/js/global-script.js
+++ src/js/global-script.js
@@ -65,12 +65,12 @@ $( document ).ready(function() {
     autoplayHoverPause: true,
     loop: true,
     navText: ['<svg width="100%" height="100%" viewBox="0 0 11 20"><path style="fill:none;stroke-width: 1px;" d="M9.554,1.001l-8.607,8.607l8.607,8.606"/></svg>', '<svg width="100%" height="100%" viewBox="0 0 11 20" version="1.1"><path style="fill:none;stroke-width: 1px;" d="M1.054,18.214l8.606,-8.606l-8.606,-8.607"/></svg>'],
-    center: true,
+    center: false,
     responsive : {
       0 : {
         items: 2,
         loop: true,
-        center: true,
+        nav: false,
       },
       480 : {
         items: 3,
